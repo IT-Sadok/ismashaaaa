784ef26e233283279dce9c9c6d8e48b81002c654
ismashaaaa <mashaderduik@gmail.com>
2025-07-23 12:20:41 +0300

    Fix JWT key decoding

--- MakeupClone/MakeupClone/MakeupClone.Infrastructure/Extensions/ServiceCollectionExtensions.cs
+++ MakeupClone/MakeupClone/MakeupClone.Infrastructure/Extensions/ServiceCollectionExtensions.cs
@@ -1,4 +1,6 @@
-﻿using MakeupClone.Application.Interfaces;
+﻿using System.Text;
+
+using MakeupClone.Application.Interfaces;
 using MakeupClone.Application.Services;
 using MakeupClone.Domain.Entities;
 using MakeupClone.Domain.Enums;
@@ -44,7 +46,7 @@ public static class ServiceCollectionExtensions
         var jwtSettings = configuration.GetSection("Jwt");
         var googleSettings = configuration.GetSection("Google");
 
-        var key = Convert.FromBase64String(jwtSettings["Key"] !);
+        var key = Encoding.UTF8.GetBytes(jwtSettings["Key"] !);
 
         services.AddAuthentication(options =>
         {
